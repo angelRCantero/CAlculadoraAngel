83e939b6198f8b9c1a0e2e9fd335e59727f4607d
angel <angelrcantero@gmail.com>
2022-11-12 11:18:23 -0300

    commit secundario
    segundo commit para entrar en sintaxis de la app de forma basica con operaciones separadas

---
+++ resources.py
@@ -0,0 +1,26 @@
+####aca va a ir el codigo de la calculadora
+##Empiezo con codigo basico para entender sintaxis basicas
+ 
+num1 = input("ingresa un numero")
+num2 = input("ingresa otro numero")
+num1 = float(num1)
+num2 = float(num2)
+print(num1, "+", num2, "=",num1+num2)
+
+num1 = input("ingresa un numero")
+num2 = input("ingresa otro numero")
+num1 = float(num1)
+num2 = float(num2)
+print(num1, "-", num2, "=",num1-num2)
+
+num1 = input("ingresa un numero")
+num2 = input("ingresa otro numero")
+num1 = float(num1)
+num2 = float(num2)
+print(num1, "*", num2, "=",num1*num2)
+
+num1 = input("ingresa un numero")
+num2 = input("ingresa otro numero")
+num1 = float(num1)
+num2 = float(num2)
+print(num1, "/", num2, "=",num1/num2)
