5cd4ca25e745d0570c6a7d1b16f6d1817117041e
angel <angelrcantero@gmail.com>
2022-11-13 19:05:53 -0300

    commit 3
    comienzo con especificaciones de tipos de funciones que realiza la culculadora,integrando variables de entrada para que se asigne una funcion a eleccion del usuario dentro del menu de mi proyecto.

--- resources.py
+++ resources.py
@@ -1,26 +1,84 @@
 ####aca va a ir el codigo de la calculadora
 ##Empiezo con codigo basico para entender sintaxis basicas
- 
-num1 = input("ingresa un numero")
-num2 = input("ingresa otro numero")
-num1 = float(num1)
-num2 = float(num2)
-print(num1, "+", num2, "=",num1+num2)
+#comienzo con estructuras basadas en un video de internet que daba 
+# informacion de como hacer una calculadora con semantica mas avanzada pero bien explicada
+import sys
 
-num1 = input("ingresa un numero")
-num2 = input("ingresa otro numero")
-num1 = float(num1)
-num2 = float(num2)
-print(num1, "-", num2, "=",num1-num2)
+num1 = 0
+num2 = 0
 
-num1 = input("ingresa un numero")
-num2 = input("ingresa otro numero")
-num1 = float(num1)
-num2 = float(num2)
-print(num1, "*", num2, "=",num1*num2)
+menu = """
+1:Sumar
+2:Restar
+3:Multiplicar
+4:Dividir
+5:Potencia
+6:Raiz
+0:Salir
+"""
 
-num1 = input("ingresa un numero")
-num2 = input("ingresa otro numero")
-num1 = float(num1)
-num2 = float(num2)
-print(num1, "/", num2, "=",num1/num2)
+def Sumar():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("la suma de los numeros es igual a: ", num1 + num2)
+    
+    
+def Restar():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("la resta de los numeros es igual a: ", num1 - num2)
+    
+    
+def Multiplicar():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("el resultado de la multiplicacion es igual a: ", num1 * num2)
+    
+    
+def Dividir():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("el resultado de la division es igual a: ", num1 / num2)
+    
+    
+def Potencia():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("el resultado de la potencia es : ", num1 ** num2)
+    
+    
+def Raiz():
+    print("ingrese el primer numero: ")
+    num1 = float(input())
+    print("ingresa el segundo numero: ")
+    num2 = float(input())
+    print("el resultado de la raiz es : ", num1 ** (1/num2) )
+    
+print("bienvenido a la calculadora basica que pidio el profe juan...")
+while True:
+    print(menu)  #el valor de 'valE' es igual al valor de la entrada del usuario
+    valE = int(input())
+    if valE == 1:
+        Sumar()
+    elif valE == 2:
+        Restar()
+    elif valE == 3:
+        Multiplicar()
+    elif valE == 4:
+        Dividir()
+    elif valE == 5:
+        Potencia()
+    elif valE == 6:
+        Raiz()
+    elif valE == 0:
+        sys.exit()
